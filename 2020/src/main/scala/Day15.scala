dc559db67f10800f4feadc2dd6b3c7f8be43f9a3
colinlcrawford <ccolin84@gmail.com>
2020-12-16 08:15:14 -0500

    switch day 15 to use int indexed array over map to trade more memory usage for better time performance

--- 2020/src/main/scala/Day15.scala
+++ 2020/src/main/scala/Day15.scala
@@ -6,24 +6,29 @@ object Day15 extends AOCDay {
   def dayNum(): Int = 15
 
   def part1(input: Array[String]): Option[String] = { 
-    val ans = playGameUntilTurn(input(0).split(",").map(_.toInt).zipWithIndex, 2020)
+    val ans = playGameUntilTurn(input(0).split(",").map(_.toInt).zipWithIndex, 2020, 100000)
     Some(ans.toString)
   }
 
   def part2(input: Array[String]): Option[String] = {
-    val ans = playGameUntilTurn(input(0).split(",").map(_.toInt).zipWithIndex, 30000000)
+    val ans = playGameUntilTurn(input(0).split(",").map(_.toInt).zipWithIndex, 30000000, 30000000)
     Some(ans.toString)
   }
 
-  def playGameUntilTurn(inputs: Array[(Int, Int)], turnsToPlay: Int): Int = {
-    var seen: Map[Int, List[Int]] = inputs.map({ case (num, turn) => (num -> List(turn + 1)) }).toMap
-    var currentTurn = seen.size + 1
+  def playGameUntilTurn(inputs: Array[(Int, Int)], turnsToPlay: Int, answerArrSize: Int): Int = {
+    var seen: Array[Array[Int]] = Array.fill(answerArrSize){ Array(-1, -1) }
+    inputs.foreach({ case (num, turn) => seen(num)(0) = turn + 1 })
+    var currentTurn = inputs.size + 1
     var prevNum = inputs.last._1
 
     while (currentTurn <= turnsToPlay) {
       val turnsSaid = seen(prevNum)
-      prevNum = if (turnsSaid.size > 1) turnsSaid.head - turnsSaid.tail.head else 0
-      seen += (if (seen.contains(prevNum)) (prevNum -> List(currentTurn, seen(prevNum).head)) else (prevNum -> List(currentTurn)))
+      prevNum = if (turnsSaid(1) != -1) turnsSaid(0) - turnsSaid(1) else 0
+      val prevSeen = seen(prevNum)
+      if (prevSeen(0) != -1) {
+        seen(prevNum)(1) = seen(prevNum)(0)
+      }
+      seen(prevNum)(0) = currentTurn
       currentTurn += 1
     }
 
