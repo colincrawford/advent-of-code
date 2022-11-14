ee701156b1f3bce5086262eed42d12402e95f986
colinlcrawford <ccolin84@gmail.com>
2022-11-13 21:21:06 -0500

    add expect testing for daily solutions

--- 2022/solutions/day1.ml
+++ 2022/solutions/day1.ml
@@ -5,3 +5,11 @@ let part2 _input = "<Part 2 answer here>"
 let solve: Day_solution.solver = fun input ->
   { part1 = (part1 input)
   ; part2 = (part2 input) }
+
+let%test_unit "Day1 outputs match" =
+  Test_helpers.solutions_match
+    ~day:1
+    ~solver:solve
+    ~expected_part1:""
+    ~expected_part2:""
+
---
+++ 2022/solutions/test_helpers.mli
@@ -0,0 +1,6 @@
+val solutions_match :
+    day:int ->
+    solver:Day_solution.solver ->
+    expected_part1: string ->
+    expected_part2: string ->
+    unit
